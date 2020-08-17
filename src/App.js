cb3be53d43ba2851d927791f2a18a3f427a6db30
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2020-08-17 22:30:08 +0700

    Add Footer and HelloWorld component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import HelloWorld from "./Components/HelloWorld";
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
+      <HelloWorld name="Dejan" />
+      <Footer />
     </div>
   );
 }
